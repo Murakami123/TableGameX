7ed06d0f05e8022b7218f11016ac3e880814c654
Murakami <snowmanappli123@gmail.com>
2018-08-17 05:02:57 +0900

    雑なシーン遷移、gitignore

---
+++ TableGameX/Assets/Scenes/Game/Script/GameSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour {
+
+	/// 戻るボタンを押したとき
+	public void OnReturnBtnTap(){
+		SceneManager.LoadScene( "Title" );
+	}
+
+}
+
+
+
---
+++ TableGameX/Assets/Scenes/Title/Script/TitleSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleSceneManager : MonoBehaviour {
+
+
+	/// 「始める」ボタンタップ
+	public void OnStartBtnTap(){
+		SceneManager.LoadScene( "Game" );
+	}
+}
